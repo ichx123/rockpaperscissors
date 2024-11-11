72148300791fe357fdf83468dd537d07177b3b63
ichx123 <ichx123@icloud.com>
2024-11-11 21:23:20 +0100

    feat: add function to get the players choice
    Added a Function, where User can input a number between
    1 and 3, to choose between Rock, Paper, Scissors.
    
    If the user inputs a invalid answer, there will be an alert
    and the option to input a value again.

--- javascript.js
+++ javascript.js
@@ -16,7 +16,6 @@ function getComputerChoice() {
     if (random > 0.33 && random <= 0.66) {
         return paper;
     }
-  
     //If random is between 0 and 0.33 
     //then return "rock"
     else if (random <= 0.33) {
@@ -27,5 +26,42 @@ function getComputerChoice() {
     else {
         return scissors;
     }
+
+}
+
+//Get Choice from Human Player - Function
+function getHumanChoice() {
+
+    //Create variable "humanChoice" to store the answer from player, initialize with null;
+    let humanChoice = null;
+    
+    //Ask the Player "Which Option do you want to take? Type in 1 for "Rock", 2 for "Paper" and 3 for "Scissors" and store the anser in variable "humanChoice"
+    humanChoice = prompt('Which Option do you want to take? Type "1" for "Rock, "2" for Paper and "3" for "Scissors".',1);
+
+    switch(humanChoice) {
+
+        //If humanChoice is 1, return rock
+        case "1":
+            return rock;
+            break;
+        
+         // If humanChoice is 2, return paper
+        case "2":
+            return paper;
+            break;
+
+        //if humanChoice is 3, return scissors
+        case "3":
+            return scissors;
+            break;
+
+        // if answer is NOT on of this options, 
+        // give message to the user that this answer is not valid
+        default: 
+            alert("This answer is not valid, please choose a number between 1 and 3.");
+            // call function again to get a valid answer
+            return getHumanChoice();
+    }
+
 
 }
