71b010b83c142e858ac931295a147f9898cc94f3
ichx123 <ichx123@icloud.com>
2024-11-14 09:51:59 +0100

    feat: add function to play a entire game
    a game consists of 5 rounds, each round is played
    with the function "playRound". This function is moved
    into the playGame-function to get access to the
    variables "humanScore" and "computerScore" which are
    defined in the "playGame"-function.
    
    A function "declareWinner" is helping with declaring
    the final winner of 5 rounds. It is possible to end
    with a tie.
    
    On the end of the "playGame"-function the variables
    "humanScore" and "computerScore" are set back to 0,
    if we want to start a new game;

--- javascript.js
+++ javascript.js
@@ -4,10 +4,7 @@ const rock = "Rock";
 const paper = "Paper";
 //Create Variable "scissors" for string "Scissors"
 const scissors = "Scissors";
-//Create Variable "humanScore" to track the wins of the playser
-let humanScore = 0;
-//Create Variable "computerScore" to track the wins of the computer
-let computerScore = 0;
+
 
 // Get Choice From Computer - Function
 function getComputerChoice() {
@@ -38,18 +35,18 @@ function getHumanChoice() {
 
     //Create variable "humanChoice" to store the answer from player, initialize with null;
     let humanChoice = null;
-    
-    //Ask the Player "Which Option do you want to take? Type in 1 for "Rock", 2 for "Paper" and 3 for "Scissors" and store the anser in variable "humanChoice"
-    humanChoice = prompt('Which Option do you want to take? Type "1" for "Rock, "2" for Paper and "3" for "Scissors".',1);
 
-    switch(humanChoice) {
+    //Ask the Player "Which Option do you want to take? Type in 1 for "Rock", 2 for "Paper" and 3 for "Scissors" and store the anser in variable "humanChoice"
+    humanChoice = prompt('Which Option do you want to take? Type "1" for "Rock, "2" for Paper and "3" for "Scissors".', 1);
+
+    switch (humanChoice) {
 
         //If humanChoice is 1, return rock
         case "1":
             return rock;
             break;
-        
-         // If humanChoice is 2, return paper
+
+        // If humanChoice is 2, return paper
         case "2":
             return paper;
             break;
@@ -61,7 +58,7 @@ function getHumanChoice() {
 
         // if answer is NOT on of this options, 
         // give message to the user that this answer is not valid
-        default: 
+        default:
             alert("This answer is not valid, please choose a number between 1 and 3.");
             // call function again to get a valid answer
             return getHumanChoice();
@@ -70,37 +67,88 @@ function getHumanChoice() {
 
 }
 
-// Play a Round - Function which takes the choices of the players and look who is winning
-function playRound(humanChoice, computerChoice) {
 
-//If HumanChoice is Scissors and computerChoice is Rock
-//If HumanChoice is Paper and CompiterChoice is Scissors
-//If HumanChoice is Rock and computerChoice is Paper
-    //Then Computer wins, therefore increment computerWins and alert the winner
-if ((humanChoice === rock && computerChoice === paper) || (humanChoice === scissors && computerChoice === rock) || (humanChoice === paper && computerChoice === scissors)) {
-    computerScore++;
-    alert(`You loose, ${computerChoice} beats ${humanChoice}!`);
-} 
 
-//If humanChoice is Rock and computerChoice is Scissors 
-//If Humanchoche is Scissors and computerChoice is Paper
-//If Humanchoice is Paper and ComputerChoice is Rock
-    //Then Player wins, increment playerWins and alert the winner
+//Play a Game - Funktion
+function playGame() {
 
-    else if ((humanChoice === rock && computerChoice === scissors) || (humanChoice === scissors && computerChoice === paper) || (humanChoice === paper && computerChoice === rock))
-{
-    humanScore++;
-    alert(`You win, ${humanChoice} beats ${computerChoice}!`);
+    //Create Variable "humanScore" to track the wins of the player
+    let humanScore = 0;
+    //Create Variable "computerScore" to track the wins of the computer
+    let computerScore = 0;
 
-} 
-//If the two choices are the same, the game ends with a tie, alert that nobodys a winner
+    //Loop for "roundsplayed" from 0 to 5
+    for (let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
+        //create Variable "humanSelection" and store the result of the function "getHumanChoice"
+        const humanSelection = getHumanChoice();
+        //create Variable "computerSelection" and store the result of the function "getComputerChoice"
+        const computerSelection = getComputerChoice();
+        //Play one round of the game with the function "playRound"
+        playRound(humanSelection, computerSelection);
 
-else {
-    alert(`Tie! ${humanChoice} vs ${computerChoice}!`);
-}
-}
+    }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    //Declare a Winner with the Function "declareWinner"
+    declareWinner(humanScore, computerScore);
 
-playRound(humanSelection, computerSelection);
+    //Set Scores to 0, if you want to start a new game
+    humanScore = 0;
+    computerScore = 0;
+
+
+    // Play a Round - Function which takes the choices of the players and look who is winning
+    function playRound(humanChoice, computerChoice) {
+
+        //If HumanChoice is Scissors and computerChoice is Rock
+        //If HumanChoice is Paper and CompiterChoice is Scissors
+        //If HumanChoice is Rock and computerChoice is Paper
+        //Then Computer wins, therefore increment computerWins and alert the winner
+        if ((humanChoice === rock && computerChoice === paper) || (humanChoice === scissors && computerChoice === rock) || (humanChoice === paper && computerChoice === scissors)) {
+            computerScore++;
+            alert(`You loose, ${computerChoice} beats ${humanChoice}!`);
+        }
+
+        //If humanChoice is Rock and computerChoice is Scissors 
+        //If Humanchoche is Scissors and computerChoice is Paper
+        //If Humanchoice is Paper and ComputerChoice is Rock
+        //Then Player wins, increment playerWins and alert the winner
+
+        else if ((humanChoice === rock && computerChoice === scissors) || (humanChoice === scissors && computerChoice === paper) || (humanChoice === paper && computerChoice === rock)) {
+            humanScore++;
+            alert(`You win, ${humanChoice} beats ${computerChoice}!`);
+
+        }
+        //If the two choices are the same, the round ends with a tie, alert that nobodys a winner
+
+        else {
+            alert(`Tie! ${humanChoice} vs ${computerChoice}!`);
+        }
+    } // end function "playRound"
+
+
+    //Declare a Winner - Function, arguments are the scores
+    function declareWinner(humanScore, computerScore) {
+
+        //If humanScore and computerScore are similar
+        if (humanScore === computerScore) {
+            //Then Nobody is a Winner
+            //Alert "Tie! Nobody wins!
+            alert("Tie! Nobody's a winner!")
+        }
+        // Else If humanScore is bigger than computerScore
+        
+        else if (humanScore > computerScore) {
+            //Then human is winner
+            //Alert "You win!"
+            alert("You win!")
+        }
+        //Else 
+        else {
+            //Computer Wins
+            //Alert "You loose! Computer has won!"
+            alert("You loose! Computer has won!")
+
+        } //end if-clause
+    } // end function declareWinner
+
+} //end function playGame
